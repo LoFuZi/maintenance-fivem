38363983bc85badc8d2e56588d1f8a663ba61dc8
lofuzi <100389264+LoFuZi@users.noreply.github.com>
2023-03-01 15:33:13 +0100

    Update config.lua

--- lfz_maintenance/config.lua
+++ lfz_maintenance/config.lua
@@ -1,7 +1,7 @@
 Config = { 
     maintenance = true, 
     autorisationMaintenance = { --these peoples are allowed to connect when maintenance
-        'steam:110000141ff6fca',
+        'steam:',
     },
     serverName = "LoFuZi",
     discordLink = "https://discord.gg/9hgGBac4bv",
