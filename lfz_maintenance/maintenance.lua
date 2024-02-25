ac7ce2c76790e3a3416c299aa35169846e2c38f5
PingouinTheDev <72406765+pingouinn@users.noreply.github.com>
2024-02-25 19:01:43 +0100

    Supressed some nested loops.
    Refactored code
    Deleted useless variables
    Changed table iterator from ipairs to pairs (never use ipairs on a non number indexed table !!!)
    Changed config to avoid nested loops killing the performance

--- lfz_maintenance/maintenance.lua
+++ lfz_maintenance/maintenance.lua
@@ -1,26 +1,22 @@
 if Config.maintenance then 
     print("MAINTENANCE : ^2Activée^0")
     function AutorisationMaintenance(joueur)
-        local allowed = false
-        for i,id in ipairs(Config.autorisationMaintenance) do
-            for x,pid in ipairs(GetPlayerIdentifiers(joueur)) do
-                if string.lower(pid) == string.lower(id) then
-                    allowed = true
-                end
+        for _, pid in pairs(GetPlayerIdentifiers(joueur)) do
+            if Config.autorisationMaintenance[string.lower(pid)] then
+                return true
             end
         end
-        return allowed
+        return false
     end
 
     local function OnPlayerConnecting(name, setKickReason, deferrals)
-        local player = source
         deferrals.defer()
         Wait(0)
     
         deferrals.update(string.format("Vérification des maintenances... ", name))
         Wait(1000)
     
-        if AutorisationMaintenance(player) then
+        if AutorisationMaintenance(source) then
             deferrals.done()
         else
             deferrals.done("\n["..Config.serverName.."] Le serveur est en maintenance, consultez les informations sur notre discord. (" ..Config.discordLink..")")
@@ -28,11 +24,7 @@ if Config.maintenance then
     
     end
     AddEventHandler("playerConnecting", OnPlayerConnecting)
-
-
 end
-
-
 
 
 
